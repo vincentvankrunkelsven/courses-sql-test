150a86b799e034c03a37b54b56d10f91e2686a6a
Filip Schouwenaars <filip@datacamp.com>
2017-02-08 10:22:20 +0100

    add reqs

--- requirements.sh
+++ requirements.sh
@@ -1,1 +1,9 @@
 # no extra requirements.
+# Define versions
+SQLWHAT_VERSION="v0.0.3"
+SQLBACKEND_VERSION="master"
+
+# Install sqlwhat and sqlbackend
+pip3 install git+https://github.com/datacamp/pythonwhat@master
+pip3 install git+https://github.com/datacamp/sqlwhat@$SQLWHAT_VERSION
+pip3 install git+https://$GITHUB_TOKEN@github.com/datacamp/sqlbackend.git/@$SQLBACKEND_VERSION
