215cb8ff0d24e1bf886c3010635f2d6d764b4f45
Michael Chow <machow@princeton.edu>
2017-02-14 11:24:41 -0500

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -31,7 +31,7 @@ rm $zipfile $tarfile
 
 # Define versions
 SQLWHAT_VERSION="v0.0.4"
-SQLBACKEND_VERSION="v0.0.11"
+SQLBACKEND_VERSION="v0.0.12"
 #
 # Install sqlwhat and sqlbackend
 pip3 install git+https://github.com/datacamp/pythonwhat@master
